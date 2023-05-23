da1189cdce39ac6a8cd3843c4157a220c11cddfd
Fairuz Kaweesa <kaweesafairuz@gmail.com>
2023-05-23 08:06:44 -0700

    Initail App publishing

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:chart_app/screens/welcome_screen.dart';
+import 'package:chart_app/screens/login_screen.dart';
+import 'package:chart_app/screens/registration_screen.dart';
+import 'package:chart_app/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+      initialRoute:WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id:(context)=>WelcomeScreen(),
+        LoginScreen.id:(context)=>LoginScreen(),
+        RegistrationScreen.id:(context)=>RegistrationScreen(),
+        ChatScreen.id:(context)=>ChatScreen(),
+      },
+    );
+  }
+}
